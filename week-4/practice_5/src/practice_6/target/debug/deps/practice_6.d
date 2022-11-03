ca73177f169ce88b1daa71adab5a71405e887a5a
GinaBriggs <gina.captain.briggs@gmail.com>
2022-11-03 17:31:54 +0100

    (Week 4 Commit)

---
+++ week-4/practice_5/src/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Documents\g.captain-briggsCSC101\week-4\practice_5\src\practice_6\target\debug\deps\practice_6.exe: src\main.rs
+
+C:\Users\USER\Documents\g.captain-briggsCSC101\week-4\practice_5\src\practice_6\target\debug\deps\practice_6.d: src\main.rs
+
+src\main.rs:
